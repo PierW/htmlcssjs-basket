ac8043b48fc0f050fda69e3e3e589a86531b1fbe
Pier.W <factory666@live.it>
2019-03-07 17:34:02 +0100

    random, getid done

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 $(document).ready(init);
 
+function getRandom(min, max){
+  var diff = (max - min) +1;
+  return Math.floor(Math.random() * diff ) + min;
+}
+
+function getId() {
+  var letters = "";
+  var numbers = "";
+  for (var i = 0; i < 3; i++) {
+    var rnd = getRandom(1,9);
+    var rndLetters = getRandom(65,90);
+    rndLetters = String.fromCharCode(rndLetters);
+    numbers += rnd;
+    letters += rndLetters;
+  }
+  var idPlayer = letters + numbers;
+  return idPlayer;
+}
 
 function init() {
-
-	// TODO
+console.log(getId());
 }
